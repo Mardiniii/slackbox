04176876343fb8d6cefa868fe477b39bbf59922d
mardiniii <sezama03@hotmail.com>
2016-05-21 15:16:23 -0500

    Add data_key_name to data clips table

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160521211212) do
     t.json     "slack_response"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+    t.string   "name"
   end
 
   add_index "data_clips", ["channel_id"], name: "index_data_clips_on_channel_id", using: :btree
@@ -79,11 +80,11 @@ ActiveRecord::Schema.define(version: 20160521211212) do
     t.string   "slack_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "team_id"
     t.string   "provider"
     t.string   "uid"
     t.string   "username"
     t.string   "image"
-    t.integer  "team_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
