5f158984eadadc31ff94111fa1cd698089c91931
alejoescobar <alejoescobac@gmail.com>
2016-05-21 22:02:58 -0500

    Remove devise registration routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
          omniauth_providers: [:slack]
 
   def self.from_omniauth(auth)
