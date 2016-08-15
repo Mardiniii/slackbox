359f9b5f92b098a5b96c2e5c005e2a0659b20d2e
mardiniii <sezama03@hotmail.com>
2016-08-15 14:49:10 -0500

    Set the routes contraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   resources :command_requests, only: [:create] do
     collection do
       get :auth_grant
     end
   end
-  root 'pages#index'
-  devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
-  get 'panel' => 'dashboard#panel'
+  resources :command_requests, only: [:create]
   resources :dashboard, only: [:show]
+
+  get 'panel' => 'dashboard#panel'
   get 'pages/index'
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+
+  end
+
 end
