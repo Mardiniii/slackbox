56e00fa16e24f24d5e39c65f3b59b69d19647475
alejoescobar <alejoescobac@gmail.com>
2016-08-15 21:31:00 -0500

    Add react on rails

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get :auth_grant
     end
   end
+  get 'hello_world', to: 'hello_world#index'
   root 'pages#index'
   devise_for :users, controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'panel' => 'dashboard#panel'
