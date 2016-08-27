a6807287d082972ff91877413f45c1f094b9cf9e
Simon S. Soriano <simon0191@gmail.com>
2016-08-27 14:07:37 -0500

    Improve save and star responses

--- app/commands/star_command.rb
+++ app/commands/star_command.rb
@@ -15,8 +15,24 @@ class StarCommand < Command
 
   def response
     if valid?
-      save
-      { text: 'Your data clip has been saved' }
+      data_clip = save
+      # Example:
+      # https://api.slack.com/docs/messages/builder?msg=%7B%22attachments%22%3A%5B%7B%22fallback%22%3A%22New%20dataclip%20created%20by%20Simon%22%2C%22color%22%3A%22%2336a64f%22%2C%22pretext%22%3A%22New%20dataclip%22%2C%22author_name%22%3A%22Simon%22%2C%22title%22%3A%22El%20titulo%20del%20dataclip%22%2C%22text%22%3A%22el%20contenido%20del%20dataclip%20%3Chttps%3A%2F%2Fasdfasf%7C%23asdf%3E%22%2C%22fields%22%3A%5B%7B%22title%22%3A%22Priority%22%2C%22value%22%3A%22High%22%2C%22short%22%3Afalse%7D%5D%2C%22footer%22%3A%22SlashBox%22%2C%22ts%22%3A1472323984%7D%5D%7D
+      {
+        response_type: 'in_channel',
+        attachments: [
+          {
+              fallback: "New starred dataclip created by #{data_clip.user.username}",
+              color: "#36a64f",
+              pretext: "New starred dataclip created by @#{data_clip.user.username}",
+              title: ":star: #{data_clip.name}",
+              text: data_clip.data,
+              footer: "SlashBox",
+              ts: data_clip.created_at.to_time.to_i,
+              mrkdwn_in: ["text"]
+          },
+        ]
+      }
     else
       invalid_arguments_response
     end
@@ -54,7 +70,7 @@ class StarCommand < Command
     tags.each do |tag|
       data_clip.tags << command_request.team.tags.where(name: tag).first_or_create
     end
-
+    data_clip
   end
 
 end
