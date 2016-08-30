dd0e15c97ea8f6577e96c8dbcff34ba920e8ee36
Simon S. Soriano <simon0191@gmail.com>
2016-08-30 14:07:47 -0500

    Add 'add to slack' button

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,7 +2,7 @@ module PagesHelper
   def slack_oauth_params
     {
       scope: 'commands',
-      client_id: '46385812515.46427344132',
+      client_id: "#{ENV['slack_client']}",
       redirect_uri: auth_grant_command_requests_url
     }.to_query
   end
